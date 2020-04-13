d537b3380b98bf3546ab12b93f61620dcd5e1e6e
Karol Szpek <56266637+karolsz15@users.noreply.github.com>
2020-04-13 18:23:07 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ const modal2 = document.getElementById("myModal2");
 const modal3 = document.getElementById("myModal3");
 const modal4 = document.getElementById("myModal4");
 const modal5 = document.getElementById("myModal5");
+const modal6 = document.getElementById("myModal6");
+
 
 // Get the button that opens the modal
 const btn1 = document.getElementById("myBtn1");
@@ -11,6 +13,8 @@ const btn2 = document.getElementById("myBtn2");
 const btn3 = document.getElementById("myBtn3");
 const btn4 = document.getElementById("myBtn4");
 const btn5 = document.getElementById("myBtn5");
+const btn6 = document.getElementById("myBtn6");
+
 
 // Get the <span> element that closes the modal
 const span1 = document.getElementsByClassName("close")[0];
@@ -18,6 +22,8 @@ const span2 = document.getElementsByClassName("close")[1];
 const span3 = document.getElementsByClassName("close")[2];
 const span4 = document.getElementsByClassName("close")[3];
 const span5 = document.getElementsByClassName("close")[4];
+const span6 = document.getElementsByClassName("close")[5];
+
 
 // Get the buttons that close the modals
 const closemodal1 = document.getElementsByClassName("closemodal")[0];
@@ -25,6 +31,8 @@ const closemodal2 = document.getElementsByClassName("closemodal")[1];
 const closemodal3 = document.getElementsByClassName("closemodal")[2];
 const closemodal4 = document.getElementsByClassName("closemodal")[3];
 const closemodal5 = document.getElementsByClassName("closemodal")[4];
+const closemodal6 = document.getElementsByClassName("closemodal")[5];
+
 
 // When the user clicks on the button, open the modal
 btn1.onclick = function() {
@@ -45,6 +53,9 @@ btn4.onclick = function() {
 
 btn5.onclick = function() {
     modal5.style.display = "block";
+}
+btn6.onclick = function() {
+  modal6.style.display = "block";
 }
 
 // When the user clicks on <span> (x), close the modal and reset slideIndex
@@ -93,6 +104,15 @@ closemodal5.onclick = function() {
     slideIndex = 1;
 }
 
+span6.onclick = function() {
+  modal6.style.display = "none";
+  slideIndex = 1;
+}
+closemodal6.onclick = function() {
+  modal6.style.display = "none";
+  slideIndex = 1;
+}
+
 // When the user clicks anywhere outside of the modal, close it and reset slideIndex
 window.onclick = function(event) {
   if (event.target == modal1) {
@@ -115,13 +135,17 @@ window.onclick = function(event) {
     modal5.style.display = "none";
     slideIndex = 1;
   }
+  if (event.target == modal6) {
+    modal6.style.display = "none";
+    slideIndex = 1;
+  }
 } 
 
 //jQuery SMOOTH SCROLLING ANIMATION
 
 $(document).ready(function(){
     // Add smooth scrolling to all links
-    $("a").on('click', function(event) {
+    $("li").on('click', function(event) {
   
       // Make sure this.hash has a value before overriding default behavior
       if (this.hash !== "") {
@@ -179,12 +203,15 @@ function showSlides(n) {
   const slides3 = document.getElementsByClassName("mySlides3");
   const slides4 = document.getElementsByClassName("mySlides4");
   const slides5 = document.getElementsByClassName("mySlides5");
+  const slides6 = document.getElementsByClassName("mySlides6");
 
   const dots1 = document.getElementsByClassName("dot1");
   const dots2 = document.getElementsByClassName("dot2");
   const dots3 = document.getElementsByClassName("dot3");
   const dots4 = document.getElementsByClassName("dot4");
   const dots5 = document.getElementsByClassName("dot5");
+  const dots6 = document.getElementsByClassName("dot6");
+
 
   if (n > slides1.length) {slideIndex = 1}
   if (n < 1) {slideIndex = slides1.length}
@@ -201,6 +228,9 @@ function showSlides(n) {
   if (n > slides5.length) {slideIndex = 1}
   if (n < 1) {slideIndex = slides5.length}
 
+  if (n > slides6.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides6.length}
+
   for (i = 0; i < slides1.length; i++) {
       slides1[i].style.display = "none";
   }
@@ -215,6 +245,9 @@ function showSlides(n) {
   }
   for (i = 0; i < slides5.length; i++) {
     slides5[i].style.display = "none";
+  }
+  for (i = 0; i < slides6.length; i++) {
+    slides6[i].style.display = "none";
   }
 
   for (i = 0; i < dots1.length; i++) {
@@ -232,12 +265,17 @@ function showSlides(n) {
   for (i = 0; i < dots5.length; i++) {
     dots5[i].className = dots5[i].className.replace(" active", "");
   }
+  for (i = 0; i < dots6.length; i++) {
+    dots6[i].className = dots6[i].className.replace(" active", "");
+  }
 
   slides1[slideIndex-1].style.display = "block";
   slides2[slideIndex-1].style.display = "block";
   slides3[slideIndex-1].style.display = "block";
   slides4[slideIndex-1].style.display = "block";
   slides5[slideIndex-1].style.display = "block";
+  slides6[slideIndex-1].style.display = "block";
+
 
   //Toggle the dots indicators
   dots1[slideIndex-1].className += " active";
@@ -245,5 +283,7 @@ function showSlides(n) {
   dots3[slideIndex-1].className += " active";
   dots4[slideIndex-1].className += " active";
   dots5[slideIndex-1].className += " active";
+  dots6[slideIndex-1].className += " active";
+
 }
   
